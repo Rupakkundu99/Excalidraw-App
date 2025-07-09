adf79a400210e16c135fc94579e3e0d1859f7d8a
Rupakkundu99 <mailupsana@gmail.com>
2025-07-09 15:31:21 +0530

    Added the Roomcanvas

--- apps/excaldiraw-app/components/Roomcanvas.tsx
+++ apps/excaldiraw-app/components/Roomcanvas.tsx
@@ -4,3 +4,12 @@ import { WS_BACKEND } from "@/config"
 import { initDraw } from "@/Draw"
 import { useEffect,useRef,useState } from "react"
 import { Canvas } from "./Canvas"
+
+
+export function RoomCanvas({roomId}:{roomId:string}){
+    const [socket,setSocket]=useState<WebSocket|null>(null)
+
+    useEffect(()=>{
+        const ws=new WebSocket(``)
+    })
+}
