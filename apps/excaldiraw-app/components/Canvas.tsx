666a48a7ebe3f5ae7ed8a6b94c5233afa7253212
Rupakkundu99 <mailupsana@gmail.com>
2025-09-06 22:08:06 +0530

    Canvas change added

--- apps/excaldiraw-app/components/Canvas.tsx
+++ apps/excaldiraw-app/components/Canvas.tsx
@@ -1,71 +1,90 @@
-import { useEffect,useRef,useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Game } from "@/Draw/Game";
-import { Circle,Pencil,RectangleHorizontalIcon } from "lucide-react";
+import { Circle, Pencil, RectangleHorizontalIcon } from "lucide-react";
 import { IconButton } from "./IconButton";
 
-
-export type Tool="circle"|"rect"|"pencil"
+export type Tool = "circle" | "rect" | "pencil";
 
 export function Canvas({
-    roomId,
-    socket
-}:{
-    roomId:string,
-    socket:WebSocket
-}){
-    const canvaRef=useRef<HTMLCanvasElement>(null)
-    const [game,setGame]=useState<Game>()
-    const [selectedTool,setSelectedTool]=useState<Tool>("circle")
+  roomId,
+  socket,
+}: {
+  roomId: string;
+  socket: WebSocket;
+}) {
+  const canvaRef = useRef<HTMLCanvasElement>(null);
+  const [game, setGame] = useState<Game>();
+  const [selectedTool, setSelectedTool] = useState<Tool>("circle");
 
-    useEffect(()=>{
-        if(canvaRef.current){
-            const g=new Game(canvaRef.current,roomId,socket)
-            setGame(g)
+  useEffect(() => {
+    if (canvaRef.current) {
+      const g = new Game(canvaRef.current, roomId, socket);
+      setGame(g);
 
-            return ()=>{
-                g.destroy()
-            }
-        }
-    },[canvaRef])   
+      return () => {
+        g.destroy();
+      };
+    }
+  }, [canvaRef]);
 
-    return <div style={{
-        height:"100vh",
-        overflow:"hidden"
-    }}>
-        <canvas ref={canvaRef} width={window.innerWidth} height={window.innerHeight}></canvas>
-        <Topbar setSelectedTool={setSelectedTool} selectedTool={selectedTool} />
+  // Add this useEffect
+  useEffect(() => {
+    if (game) {
+      game.setTool(selectedTool);
+    }
+  }, [selectedTool, game]);
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        overflow: "hidden",
+      }}
+    >
+      <canvas
+        ref={canvaRef}
+        width={window.innerWidth}
+        height={window.innerHeight}
+      ></canvas>
+      <Topbar setSelectedTool={setSelectedTool} selectedTool={selectedTool} />
     </div>
+  );
 }
 
-function Topbar({selectedTool,setSelectedTool}:{
-    selectedTool:Tool,
-    setSelectedTool:(s:Tool)=>void
-}){
-    return <div className="top-2.5 left-2.5 fixed">
-        <div className="flex gap-1.5">
-            <IconButton
-            onClick={()=>{
-                setSelectedTool("pencil")
-            }}
-            activated={selectedTool==='pencil'}
-            icon={<Pencil />}
-            />
+function Topbar({
+  selectedTool,
+  setSelectedTool,
+}: {
+  selectedTool: Tool;
+  setSelectedTool: (s: Tool) => void;
+}) {
+  return (
+    <div className="top-2.5 left-2.5 fixed">
+      <div className="flex gap-1.5">
+        <IconButton
+          onClick={() => {
+            setSelectedTool("pencil");
+          }}
+          activated={selectedTool === "pencil"}
+          icon={<Pencil />}
+        />
 
-            <IconButton
-            onClick={()=>{
-                setSelectedTool("rect")
-            }}
-            activated={selectedTool==='rect'}
-            icon={<RectangleHorizontalIcon />}
-            />
+        <IconButton
+          onClick={() => {
+            setSelectedTool("rect");
+          }}
+          activated={selectedTool === "rect"}
+          icon={<RectangleHorizontalIcon />}
+        />
 
-            <IconButton
-            onClick={()=>{
-                setSelectedTool("circle")
-            }}
-            activated={selectedTool==='circle'}
-            icon={<Circle/>}
-            />
-        </div>
+        <IconButton
+          onClick={() => {
+            setSelectedTool("circle");
+          }}
+          activated={selectedTool === "circle"}
+          icon={<Circle />}
+        />
+      </div>
     </div>
+  );
 }
