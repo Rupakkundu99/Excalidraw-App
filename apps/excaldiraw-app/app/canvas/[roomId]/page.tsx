3a8f114b37b2e49b8dc79db5633911d8ca2b4c7c
Rupakkundu99 <mailupsana@gmail.com>
2025-09-06 22:02:24 +0530

    Added the frontend functionality

---
+++ apps/excaldiraw-app/app/canvas/[roomId]/page.tsx
@@ -0,0 +1,12 @@
+import { RoomCanvas } from "@/components/Roomcanvas";
+
+export default async function CanvasPage({ params }: {
+    params: {
+        roomId: string
+    }
+}) {
+    const roomId = (await params).roomId;
+
+    return <RoomCanvas roomId={roomId} />
+   
+}
--- apps/excaldiraw-app/components/Roomcanvas.tsx
+++ apps/excaldiraw-app/components/Roomcanvas.tsx
@@ -1,5 +1,4 @@
 "use client"
-
 import { WS_BACKEND } from "@/config"
 import { initDraw } from "@/Draw"
 import { useEffect,useRef,useState } from "react"
@@ -10,6 +9,26 @@ export function RoomCanvas({roomId}:{roomId:string}){
     const [socket,setSocket]=useState<WebSocket|null>(null)
 
     useEffect(()=>{
-        const ws=new WebSocket(``)
+        const ws=new WebSocket(`${WS_BACKEND}`)
+
+
+        ws.onopen=()=>{
+            setSocket(ws);
+            const data=JSON.stringify({
+                type:"join_room",
+                roomId
+            })
+            ws.send(data)
+        }
     })
+
+    if(!socket){
+        return <div>
+            Connecting to server....
+        </div>
+    }
+    return <div>
+        <Canvas roomId={roomId} socket={socket} />
+    </div>
+    
 }
